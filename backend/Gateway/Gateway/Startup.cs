54bf570c66e81471a1616c3e2ecc2bfa9b16ed35
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-10-28 22:38:13 -0300

    feat: Adicionado Ocelot TODO: precisa testar pelo menos 2 api's com ele pra ver se funciona TODO: averiguar se precisa adicionar polly

---
+++ backend/Gateway/Gateway/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway;
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+    
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureService(IServiceCollection services)
+    {
+        services.AddOcelot(_configuration);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.Map("/", async context =>
+            {
+                await context.Response.WriteAsync("Api Gateway Funcionando...");
+            });
+        });
+
+        app.UseOcelot().Wait();
+    }
+}
