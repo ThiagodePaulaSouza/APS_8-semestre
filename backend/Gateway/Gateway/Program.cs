e42fc64f6fe015307bf6a402ca831b1e859b78e3
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-10-29 00:43:04 -0300

    adicionado configurações da api gateway

---
+++ backend/Gateway/Gateway/Program.cs
@@ -0,0 +1,32 @@
+using Ocelot.DependencyInjection;
+
+namespace Gateway;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((host, config) =>
+            {
+                config
+                    .SetBasePath(host.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{host.HostingEnvironment.EnvironmentName}.json", optional: true,
+                        reloadOnChange: true)
+                    .AddJsonFile($"configuration.{host.HostingEnvironment.EnvironmentName}.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s => {
+                s.AddOcelot();
+            })
+            // .ConfigureLogging((hostingContext, logging) =>
+            // {
+            //     //add your logging
+            // })
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
